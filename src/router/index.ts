4d0bfda73178a144a70684bbbc8371aa36b462b0
invierno <1354002980@qq.com>
2020-12-27 12:58:42 +0800

    添加 404 页面

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,14 @@
 import Vue from 'vue';
 import VueRouter, {RouteConfig} from 'vue-router';
-import Home from '../views/Home.vue';
 import Money from '@/views/Money.vue';
 import Labels from '@/views/Labels.vue';
 import Statistics from '@/views/Statistics.vue';
+import NotFound from '@/views/NotFound.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+
   {
     path:'/',
     redirect:'/money'
@@ -36,6 +24,10 @@ const routes: Array<RouteConfig> = [
   {
     path:'/statistics',
     component:Statistics
+  },
+  {
+    path:'*',
+    component:NotFound
   }
 ];
 
