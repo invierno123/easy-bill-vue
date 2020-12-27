d25348b40ba7bb11451930fc46b7c3e4faa30d21
invierno <1354002980@qq.com>
2020-12-27 20:28:49 +0800

    修正错误，添加导航样式

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -15,14 +15,14 @@ name: "Layout"
 
 <style lang="scss" scoped>
 .nav-wrapper{
-  border: 1px solid #6ac8f0;
+
   display: flex;
   flex-direction: column;
   height: 100vh;
 }
 .content{
   overflow: auto;
-  border: 1px solid lavenderblush;
+
   flex-grow: 1;//尽量把所有高度都给content
 }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,30 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">记账</router-link>
-    |
-    <router-link to="/labels">标签</router-link>
-    |
-    <router-link to="/statistics">统计</router-link>
-  </div>
+  <nav>
+    <router-link to="/money" class="item">
+      <Icon name="money bag"/>
+      记账
+    </router-link>
+    <router-link to="/labels" class="item">
+      <Icon name="Label"/>
+      标签
+    </router-link>
+    <router-link to="/statistics" class="item">
+      <Icon name="statistics"/>
+      统计
+    </router-link>
+  </nav>
 </template>
 <script lang="ts">
+// const importAll=(requireContext: __WebpackModuleApi.RequireContext)=>requireContext.keys().forEach(requireContext);
+// try {
+//   importAll(require.context('../assets/icons',true,/\.svg$/));
+// } catch (error){
+//   console.log(error)
+// }
 
-import x from '@assets/icons/Label.svg';
-
-console.log(x);
+// import x from '@/assets/icons/Label.svg';
+//
+// console.log(x);
 
 
 export default {
@@ -20,7 +33,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.nav{
-  border: 1px solid lightcoral;
+
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0.25);
+  flex-direction: row;
+  font-size: 12px;
+
+  > .item {
+    padding: 2px 0;
+    width: 33.3333%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
 }
 </style>
