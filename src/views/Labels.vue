bc92cb0de0f6dc545aef86189abd4699dc006fb7
invierno <1354002980@qq.com>
2020-12-27 11:19:05 +0800

    创建导航栏

---
+++ src/views/Labels.vue
@@ -0,0 +1,13 @@
+<template>
+<div>Labels
+<Nav/>
+</div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,12 @@
+<template>
+<div>Statistics
+<Nav/></div>
+</template>
+
+<script lang="ts">
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
