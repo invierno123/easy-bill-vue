17ea714bf2807f263c7699433f8ba027f1419ab5
invierno <1354002980@qq.com>
2021-01-10 20:49:05 +0800

    封装model，基本完成记账页面

---
+++ src/custorm.d.ts
@@ -0,0 +1,8 @@
+type RecordItem = {
+  tags: string[];
+  types: string;
+  notes: string;
+  amount: number;
+  createdTime?: Date;//类/构造函数,?表示created可以不存在
+
+}
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout class-prefix="layout">
-    {{record}}
+
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
     <Types :value.sync="record.types"/>
     <Notes @update:value="updateNotes"/>
@@ -15,37 +15,37 @@ import Notes from '@/components/Money/Notes.vue';
 import Tags from '@/components/Money/Tags.vue';
 import Vue from 'vue';
 import {Component, Watch} from 'vue-property-decorator';
+import {model} from '@/model';
 
-type Record = {
-  tags: string[];
-  types: string;
-  notes: string;
-  amount: number;
 
-}
+
+const recordList  = model.fetch()
+
 @Component({
   components: {Tags, Notes, Types, NumberPad}
 })
 export default class Money extends Vue {
-
   tags = ['衣', '食', '住', '行'];
-  record: Record={tags:[],notes:'',types:'-',amount:0}
-recordList: Record[]=[]
+  record: RecordItem = {tags: [], notes: '', types: '-', amount: 0};
+
+
   updateTags(value: string[]) {
-    this.record.tags=value
+    this.record.tags = value;
   }
 
   updateNotes(value: string) {
-    this.record.notes=value
+    this.record.notes = value;
   }
-  saveRecord(){
-    const newRecord=JSON.parse(JSON.stringify(this.record)) //深拷贝,先变成字符串再重新创造一个对象
+
+  saveRecord() {
+    const newRecord: RecordItem = model.clone(this.record); //深拷贝,先变成字符串再重新创造一个对象
+    newRecord.createdTime = new Date();
     this.recordList.push(newRecord);
-    console.log(this.recordList);
   }
+
   @Watch('recordList')
-  onRecordListChange(){
-    window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
+  onRecordListChange() {
+    model.save(recordList)
   }
 }
 </script>
