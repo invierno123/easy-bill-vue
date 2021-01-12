64dcd79dc928b451418f09a7db2ff014c5374e19
invierno <1354002980@qq.com>
2021-01-12 14:27:42 +0800

    标签跳转 && 发现bug"Avoid using non-primitive value as key, use string/number value instead. "和“Duplicate keys detected: '[object Object]'. This may cause an update error.”

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,15 +1,27 @@
 <template>
-<Layout>
-  编  辑
-</Layout>
+  <Layout>
+    编 辑
+  </Layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import {Component} from 'vue-property-decorator'
-@Component
-export default class EditLabel extends Vue{
+import {Component} from 'vue-property-decorator';
+import {tagListModel} from '@/models/tagListModel';
 
+@Component
+export default class EditLabel extends Vue {
+  created() {//注意这里的created函数并不是自定义的,是钩子函数
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags=tagListModel.data;
+    const tag=tags.filter(t=>t.id===id)[0];
+    if(tag){
+      console.log(tag);
+    }else{
+      this.$router.replace('/404');//为了路径返回用replace而不用push
+    }
+  }
 }
 </script>
 
