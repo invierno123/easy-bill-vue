b384d87ca95751b5ded59bcad19fa10422c352fc
invierno <1354002980@qq.com>
2021-02-08 21:24:02 +0800

    部署网页到easy-bill-vue-website

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,9 @@ const path=require('path')
 
 module.exports = {
   lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/easy-bill-vue-website/'
+    : '/',
 chainWebpack:config=>{
     const dir=path.resolve(__dirname,'src/assets/icons')
   config.module
